cced8e7bb4eb9682d5ff6953539a894e9c0fdc21
yagnesh2004 <93670699+yagnesh2004@users.noreply.github.com>
2023-03-12 23:15:12 +0530

    Js file is updated using looping

--- f.js
+++ f.js
@@ -2,7 +2,29 @@ const relation =['Friends','Love','Atraction','Marrige','Enimy','Sister','Bestie
 
 
 relationText = document.querySelector(".result span");
-inputField =document.querySelector(".input-1 input")
+inputField_one =document.querySelector(".input-1 input");
+inputField_two =document.querySelector(".input-2 input")
+const flamesButoon = document.querySelector(".flame-button");
 
-let word = inputField.value;
-prompt(word);
+
+const flameIT = () => {
+    let word_1 = inputField_one.value.toLowerCase().split(" ");
+    let word_2 = inputField_two.value.toLowerCase().split(" ");
+    let final_array=[];
+
+    alert(word_1);
+    alert(word_2);
+    for(let i;i<word_1.length ;i++){
+        final_array.appendchild(word_1[i])
+        alert(final_array[i]);
+       
+    }
+    alert(final_array);
+
+}
+
+flamesButoon.addEventListener('click',flameIT);
+
+
+
+
